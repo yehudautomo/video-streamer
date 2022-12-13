c9dab7058b163e370814d59f27e6f890006d6ef8
Yehuda Dani Utomo <110799267+yeyebumpp@users.noreply.github.com>
2022-12-14 02:50:27 +0700

    update

---
+++ resources/pages/AppPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Test
+    </div>
+</template>
+
+<script>
+export default {
+    name: "app.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/posts', \App\Http\Controllers\PostController::class);
+
+
